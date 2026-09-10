24d93659f84b374e98a4b3009d437fa5fc62a54c
U-BSD\469303 <469303@SHS-HYBF6X3.beaverton.k12.or.us>
2026-09-10 10:08:57 -0700

    Palindrome

---
+++ .bash_history
@@ -0,0 +1,105 @@
+ls
+emacs filename.cpp
+emacs palindrome.cpp
+g++ palindrome.cpp -o palindrome
+emacs palindrome.cpp
+g++ palindrome.cpp -o assignment1
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+./Palindrome
+emacas palindrome.cpp
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+prj0icdQQCqQSV1218Xali6rkXG7pG3bAgoAewNBPvTxP1BqCFfRPQloWuKo6N2bcjmN5n2fHpbGODEAa
+./Palindrome
+prj0icdQQCqQSV1218Xali6rkXG7pG3bAgoAewNBPvTxP1BqCFfRPQloWuKo6N2bcjmN5n2fHpbGODEA
+./Palindrome
+emacs Palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+emacs palindrome.cpp
+emacs palindrome.cpp
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+emacs palindrome.cpp
+emacs Palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+./Palindrom
+./Palindrome
+emacs Palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+emacs Palindrome.cppp
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+emacs palindrome.cpp
+
+./Palindrome
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+g++ palindrome.cpp
+emacs palindrome.cpp
+emacs palindrome.cpp
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+emacs palindrome.cpp
+g++ palindrome.cpp -o Palindrome
+./Palindrome
+g++ palindrome.cpp -o Palindrome
+g++ palindrome.cpp -o Palindrme
+./Palindrome
+g++ palindrome.cpp -o Palindrome
+emacs Palindrome
+emacs Palindrome.cpp
+git add .
+git init
+git remote add origin https://github.com/HassanFakhry/Galbraith26-27.git
+git add .
+git commit -m "Add palindrome program"
+git branch -M main
+git push -u origin main
+git pull origin main --allow-unrelated-histories
+git add .
+git push -u origin main
+Galbraith password
+Galbraith password
+Convert to PDF
+git push -u origin main
+git push -u origin master
+git add .
+git push -u origin main
+git branch
+git pull origin main
+git config pull.rebase false
+git pull origin main
+ls
+git clone https://github.com/HassanFakhry/Galbraith26-27/
+ls
+cp palindrome.cpp ./Galbraith26-27
+cp palindrome.cpp ./Galbraith26-27/palindrome.cpp
+ls
+cd Galbraith26-27
+ls
+git add .
+git commit -m "Palindrome d1"
+git push origin main
--- palindrome.cpp
+++ palindrome.cpp
@@ -1,4 +1,11 @@
+// Hassan Fakhhry - 10:06 AM, Sep 10 2026
+// In this Palindrome project we check the first 80 characters a user inputs,
+// After filtering out the noise (Spaces/Punctuation), make all characters lowercase
+// Then, reverse that string, and check if the filtered string and the reversed filtered strong are the same
+// If they are, then print out "Palindrome!"
+// Otherwise, print Not a Palindrome.
 
+// Imports
 
 #include <iostream>
 #include <cctype>
@@ -8,20 +15,43 @@
 
 using namespace std;
 
+// There is only one function in this program, the main function.
+
 int main() {
-  char str[81];
-  char strpal[81];
-  cin.get(str, 81);
-  int size = strlen(str);
-  cout << size << endl; 
-  char final[size];
-  for (int i = 0; i < size; i++) {
-      strpal[size - 1 - i] = str[i];
+
+  // Pre defining our three c-arrays (81 because you need to include 80 characters max + the 1 '\0' character.
+  char prestr[81]; // The c-array that takes every user input (including Whitespace/punc)
+  char strpal[81]; // The c-array that will end up being reversed
+  char str[81]; // The c-array that takes the original user input (removing whitespace/punc)
+  cin.get(prestr, 81); // Max 81 characters allowed in this input buffer
+
+  int preSize = strlen(prestr); // Get the original size of the string
+  int size = 0; // Initializing the size of the string (truncated)
+  //  cout << prestr << endl;
+  for (int i = 0; i < preSize; i++) { // For each character, up until the end of the c-string
+    // cout << prestr[i] << endl; 
+    if (isalnum(prestr[i])) { // If the character is a number or letter
+      // This was my original approach, but I knew it would be tedious to write every punctuation type, so I researched and asked Gemini about this, and found out that this would remove all punctuation. if (prestr[i] != ' ' && prestr[i] != '.' && prestr[i] != '!') {
+      str[size] = std::tolower(prestr[i]); // add it to the new string as a lowercase letter
+      size++; // add to the true size of the string
+    }
+    // else cout << "poopy" << endl; 
   }
-  strpal[size] = '\0';
+
+  str[size] = '\0'; // Gemini reminded me to incorporate this, I was running in circles trying to find where my problem was and then when I asked Gemini it told me this was the problem.
+  //   cout << size << endl;
+  //  cout << str << endl;
+  // cout << size << endl;
+  
+  for (int i = 0; i < size; i++) { // For loop to make it so that until the end of the true strings length, 
+    strpal[size - i - 1] = str[i]; // Reverse the character order (-i) and account for null char (-1)
+  } 
+  strpal[size] = '\0'; 
   
   cout << strpal << endl; 
-  cout << "finish loop" << endl;
+  // cout << "finish loop" << endl;
+  if (strcmp(str, strpal) == 0) cout << "Palindrome!" << endl; // if the vals are the same
+  else cout << "Not a palindrome." << endl; // otherwise
  
   return 0;
 }
