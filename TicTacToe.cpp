ef3257f6f7b7e83723e8664e021ccef984db5b61
U-BSD\469303 <469303@SHS-HYBF6X3.beaverton.k12.or.us>
2026-09-23 10:24:42 -0700

    TicTacToe

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,4 +1,11 @@
-    #include <iostream>
+
+// Hassan Fakhry - 10:16 AM, Sep 23 2026
+// In this program we play a simple game of TicTacToe.
+// You can play as many times as you would like. The game will always start with player "X"
+// There are 4 main functions, Main (initalizes the program, defines all variables such as Wins, current player, game over, and winner)
+
+
+#include <iostream>
     #include <cstring>
     #include <cctype>
     #include <string>
@@ -9,7 +16,9 @@
     void clean_board(char grid[4][4]);
     void play_again(char grid[4][4], bool &gameOver, int &turn);
     void run_turn(char grid[4][4], char cp, int &turn, bool &winner, int &Xwins, int &Owins, int &Ties, bool &gameOver);
-    
+
+// The main function initializes the program, builds the original board, all variables that are needed across the other functions
+
     int main() {
         char grid[4][4] = {
             {' ', '1', '2', '3'},
@@ -37,9 +46,15 @@
         return 0;
     }
     
-    void run_turn(char grid[4][4], char cp, int &turn, bool &winner, int &Xwins, int &Owins, int &Ties, bool &gameOver) {
+// The run turn takes in the main turn to turn logic, it tells the user its their turn, then asks them for an input on the board, validates their input and then places it.
+// Once the placement is on the board, it checks to see if it has triggered a win/tie.
+// If it did, then it calls the other functions. (Play again?) and (Clear board)
+
+void run_turn(char grid[4][4], char cp, int &turn, bool &winner, int &Xwins, int &Owins, int &Ties, bool &gameOver) {
+
         char input[3];
         bool not_stopped;
+	
         cout << "Player " << cp << " It's your turn!";
         cout << '\n';
     
@@ -57,17 +72,19 @@
             (input[0] != '1' && input[0] != '2' && input[0] != '3')) {
             cout << "Please type a coordinate such as 3a or 1b" << endl;
         } else {
-            int row = input[1] - 'a' + 1;
+	  int row = input[1] - 'a' + 1; // Since the letter (a b and c) are just (0, 1 and 2) values above the letter a in asix, by doing this we can see the value of it as if it was a number.
             int col = input[0] - '0';
             
             if (grid[row][col] == ' ') {
                 grid[row][col] = cp;
-                turn++;
+                turn++; // Check to see if the location is empty or if it is already taken
             }
             else {
                 cout << "please stop being stupid, this spot is taken." << endl;
             }
-            
+
+	    // If 3 in a row in any direction
+	    
             if ((grid[row][1] == cp && grid[row][2] == cp && grid[row][3] == cp) ||
                 (grid[1][col] == cp && grid[2][col] == cp && grid[3][col] == cp) ||
                 (grid[1][1] == cp && grid[2][2] == cp && grid[3][3] == cp) ||
@@ -77,14 +94,14 @@
     
                 if (cp == 'X') Xwins++;
                 if (cp == 'O') Owins++;
-                
+                // Output scoreboard
                 cout << "X WINS: " << Xwins << endl;
                 cout << "O WINS: " << Owins << endl;
                 cout << "TIES: " << Ties << endl;
                 
                 winner = true;
                 play_again(grid, gameOver, turn);
-            } else {
+            } else { // If no win, check if there is a tie.
                 not_stopped = true;
                 while (not_stopped) {
                     for (int i = 1; i < 4; i++) {
@@ -92,7 +109,7 @@
                             if (grid[i][j] == ' ') not_stopped = false;
                         }
                     }
-                    if (not_stopped) {
+                    if (not_stopped) { // Run through every spot, if there is a letter then stop the loop. If there is no stop, that means that the entire board is filled.
                         cout << "The game ended in a tie. " << endl;
                         
                 
@@ -102,7 +119,7 @@
                         cout << "X WINS: " << Xwins << endl;
                         cout << "O WINS: " << Owins << endl;
                         cout << "TIES: " << Ties << endl;
-                        
+			// Ask the user if they'd like to play again now that the game is over.
                         play_again(grid, gameOver, turn);
                         winner = true;
                     }
@@ -118,25 +135,28 @@
             }
         }
     }
-    
+// For every spot in the board, set it to an empty value.
+
+
+// Since the game is over, ask the user if they would like to play again.
     void play_again(char grid[4][4], bool &gameOver, int &turn) {
         bool no_valid_answer = true;
         char input[10];
-        while (no_valid_answer) {
+        while (no_valid_answer) { // Make sure the user types a valid input
             cout << "Would you like to play again? (y/n) ";
             cin >> input;
-            char fixed_input = tolower(input[0]);
-            if (fixed_input == 'y') {
+            char fixed_input = tolower(input[0]); // (standardize the inputs for lowercase rather than Yy Nn)
+            if (fixed_input == 'y') { // assisted by google
                 clean_board(grid);
                 no_valid_answer = false;
                 turn = 0;
             }   
-            else if (fixed_input == 'n') {
+            else if (fixed_input == 'n') { // Call the end of the game 
                 cout << "Goodbye! Thank you for playing.";
                 gameOver = true;
                 no_valid_answer = false;
             }
-            else {
+            else { // Tell them to repeat their answer.
                 cout << "Your respose is not proper, please send either Y or N.";
             }
         }
